ec1c5050831396e224d7cdba7efd06e9bb3ecec5
Volodymyr Donets <volodons@gmail.com>
2023-07-19 12:53:57 +0300

    Create UserList component

---
+++ src/components/UserList.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function UserList(): JSX.Element {
+  return (
+    <>
+      <h1>This is User List!</h1>
+      <ul>
+        <li>User 1</li>
+        <li>User 2</li>
+        <li>User 3</li>
+        <li>User 4</li>
+        <li>User 5</li>
+      </ul>
+    </>
+  );
+}
+
+export default UserList;
