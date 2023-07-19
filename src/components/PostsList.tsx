739d31b938797b21773070873f144d28f2c2634d
Volodymyr Donets <volodons@gmail.com>
2023-07-19 12:51:18 +0300

    Create PostsList component

---
+++ src/components/PostsList.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function PostsList(): JSX.Element {
+  return (
+    <>
+      <h1>This is Posts List!</h1>
+      <ul>
+        <li>Post 1</li>
+        <li>Post 2</li>
+        <li>Post 3</li>
+        <li>Post 4</li>
+        <li>Post 5</li>
+      </ul>
+    </>
+  );
+}
+
+export default PostsList;
