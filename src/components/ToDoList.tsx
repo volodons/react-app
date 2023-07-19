8ae65a0ca6d55a15da0fc3575e858e866cec8cd2
Volodymyr Donets <volodons@gmail.com>
2023-07-19 12:58:28 +0300

    Create ToDoList component

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ToDoList(): JSX.Element {
+  return (
+    <>
+      <h1>This is ToDo List!</h1>
+      <ul>
+        <li>ToDo Item 1</li>
+        <li>ToDo Item 2</li>
+        <li>ToDo Item 3</li>
+        <li>ToDo Item 4</li>
+        <li>ToDo Item 5</li>
+      </ul>
+    </>
+  )
+}
+
+export default ToDoList;
