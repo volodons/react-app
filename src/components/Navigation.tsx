35b908adf30d03a24615600ae27b2a8e6a559314
Volodymyr Donets <volodons@gmail.com>
2023-07-19 13:03:00 +0300

    Create Navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Navigation(): JSX.Element {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="#">Navigation</a>
+        </li>
+        <li>
+          <a href="#">Posts List</a>
+        </li>
+        <li>
+          <a href="#">ToDo List</a>
+        </li>
+        <li>
+          <a href="#">User List</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Navigation;
