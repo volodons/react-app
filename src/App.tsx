a23320ec458e880ec7c16bde39d887dd6ec75478
Volodymyr Donets <volodons@gmail.com>
2023-07-19 15:42:07 +0300

    Fix typos in module imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { PostsList } from "./components/PostsList";
-import { UserList } from "./components/UserList";
-import { ToDoList } from "./components/ToDoList";
+import PostsList from "./components/PostsList";
+import UserList from "./components/UserList";
+import ToDoList from "./components/ToDoList";
 
 function App(): JSX.Element {
   return (
