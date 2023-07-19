8a1096ff8a1c0654f116f50999066c8cfd912ab4
Volodymyr Donets <volodons@gmail.com>
2023-07-19 16:28:03 +0300

    Add nav tag to Navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,17 +4,19 @@ import { Link } from "react-router-dom";
 function Navigation(): JSX.Element {
   return (
     <>
-      <ul>
-        <li>
-          <Link to="/">Posts List</Link>
-        </li>
-        <li>
-          <Link to="/todos">ToDo List</Link>
-        </li>
-        <li>
-          <Link to="/users">User List</Link>
-        </li>
-      </ul>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Posts List</Link>
+          </li>
+          <li>
+            <Link to="/todos">ToDo List</Link>
+          </li>
+          <li>
+            <Link to="/users">User List</Link>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
