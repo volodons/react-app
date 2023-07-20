f7713e479abed3a5ed99c5c473c114d60bc9a2e7
Volodymyr Donets <volodons@gmail.com>
2023-07-20 10:57:15 +0300

    Separate ToDoList into components

---
+++ src/components/toDo/ToDoItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ToDoItem(): JSX.Element {
+  return (
+    <>
+      <li>ToDo 1</li>
+      <li>ToDo 2</li>
+      <li>ToDo 3</li>
+      <li>ToDo 4</li>
+      <li>ToDo 5</li>
+    </>
+  );
+}
+
+export default ToDoItem;
--- src/components/toDo/ToDoList.tsx
+++ src/components/toDo/ToDoList.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Navigation from "../layout/Navigation";
+import List from "../common/List";
+import ToDoItem from "./ToDoItem";
 
 function ToDoList(): JSX.Element {
   return (
     <>
       <Navigation />
       <h1>This is ToDo List!</h1>
-      <ul>
-        <li>ToDo Item 1</li>
-        <li>ToDo Item 2</li>
-        <li>ToDo Item 3</li>
-        <li>ToDo Item 4</li>
-        <li>ToDo Item 5</li>
-      </ul>
+      <List>
+        <ToDoItem />
+      </List>
     </>
   );
 }
