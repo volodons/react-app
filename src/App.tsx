30cf14bb8cdb7c9d752c71924806364ef8a4ca12
Volodymyr Donets <volodons@gmail.com>
2023-07-21 08:53:46 +0300

    Implement NotFoundPage

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import PostsList from "./components/post/PostsList";
 import UsersList from "./components/user/UsersList";
 import ToDosList from "./components/toDo/ToDosList";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App(): JSX.Element {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<PostsList />} />
+          <Route path="/" element={<PostsList />} />
           <Route path="/users" element={<UsersList />} />
           <Route path="/todos" element={<ToDosList />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,13 @@
+function NotFoundPage(): JSX.Element {
+  return (
+    <>
+      <h1>Error 404 - Page Not Found</h1>
+      <p>
+        Sorry, the page you requested was not found. Please check if the URL you
+        entered is correct or return to the main page.
+      </p>
+    </>
+  );
+}
+
+export default NotFoundPage;
