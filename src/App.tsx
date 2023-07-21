013e1f6fa7f29f3ec45f9c0a80de01b53aa66d9e
Volodymyr Donets <volodons@gmail.com>
2023-07-21 09:14:58 +0300

    Implement ToDosPage for all ToDo components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostsPage from "./pages/PostsPage";
 import UsersList from "./components/user/UsersList";
-import ToDosList from "./components/toDo/ToDosList";
+import ToDosPage from "./pages/ToDosPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
 function App(): JSX.Element {
@@ -13,7 +13,7 @@ function App(): JSX.Element {
           <Route path="/" element={<HomePage />} />
           <Route path="/posts" element={<PostsPage />} />
           <Route path="/users" element={<UsersList />} />
-          <Route path="/todos" element={<ToDosList />} />
+          <Route path="/todos" element={<ToDosPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Router>
