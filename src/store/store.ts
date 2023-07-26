351e10559f61ed7a8e27d42b98c41c33293bc4ac
Volodymyr Donets <volodons@gmail.com>
2023-07-26 18:29:40 +0300

    Configure store to work with RTK Query API

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
+import { jsonPlaceholderApi } from "../api/jsonPlaceholderAPI";
 import postsReducer from "../reducers/postsReducer";
 import usersReducer from "../reducers/usersReducer";
 import toDosReducer from "../reducers/toDosReducer";
 
 const store = configureStore({
   reducer: {
-    posts: postsReducer,
-    users: usersReducer,
-    todos: toDosReducer,
+    [jsonPlaceholderApi.reducerPath]: jsonPlaceholderApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(jsonPlaceholderApi.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
 
 export default store;
