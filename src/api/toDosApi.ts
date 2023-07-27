c9e2e4c67dc217c5b7ef8114f81c40ec83b2e22b
Volodymyr Donets <volodons@gmail.com>
2023-07-27 20:13:33 +0300

    Inject toDosApi into baseApi

--- src/api/toDosApi.ts
+++ src/api/toDosApi.ts
@@ -1,9 +1,6 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { baseApi } from "./baseApi";
 
-export const toDosApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: "https://jsonplaceholder.typicode.com/",
-  }),
+const toDosApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getToDos: builder.query({
       query: () => "todos",
