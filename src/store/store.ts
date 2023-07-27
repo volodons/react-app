21460c1f070d3c697f6affff50423b34d302db73
Volodymyr Donets <volodons@gmail.com>
2023-07-27 20:14:41 +0300

    Reconfigure store for API

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,13 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
-import { postsApi } from "../api/postsApi";
-import { toDosApi } from "../api/toDosApi";
-import { usersApi } from "../api/usersApi";
-import postsReducer from "../reducers/postsReducer";
-import toDosReducer from "../reducers/toDosReducer";
-import usersReducer from "../reducers/usersReducer";
+import { baseApi } from "../api/baseApi";
 
 const store = configureStore({
   reducer: {
-    [postsApi.reducerPath]: postsApi.reducer,
-    [toDosApi.reducerPath]: toDosApi.reducer,
-    [usersApi.reducerPath]: usersApi.reducer,
-    posts: postsReducer,
-    toDos: toDosReducer,
-    users: usersReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(postsApi.middleware)
-      .concat(toDosApi.middleware)
-      .concat(usersApi.middleware),
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
