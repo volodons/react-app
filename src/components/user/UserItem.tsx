8e5e0ca5f6f4a8e42d788a3bce338069aee52519
Volodymyr Donets <volodons@gmail.com>
2023-08-02 14:11:46 +0300

    Reconfigure UserItem.tsx to work with Redux Thunk

--- src/components/user/UserItem.tsx
+++ src/components/user/UserItem.tsx
@@ -1,7 +1,15 @@
-import { useGetUsersQuery } from "../../api/usersApi";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers } from "../../api/usersApi";
 
 function UserItem(): JSX.Element {
-  const { data: users = [], error } = useGetUsersQuery();
+  const users = useSelector((state) => state.users.data);
+  const error = useSelector((state) => state.users.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
 
   if (error) {
     return <div>Error loading users</div>;
@@ -11,7 +19,7 @@ function UserItem(): JSX.Element {
     <>
       {users.map((user) => (
         <li key={user.id}>
-          User: {user.name}, Username: {user.username}
+          Name: {user.name} Username: {user.username}
         </li>
       ))}
     </>
