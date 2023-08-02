8f6f1b9d6b726979afacb7a9a8104110fdbf043f
Volodymyr Donets <volodons@gmail.com>
2023-08-02 14:07:14 +0300

    Reconfigure usersApi.ts to work with Redux Thunk

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,11 +1,26 @@
-import { baseApi } from "./baseApi";
+import axios from "axios";
 
-const usersApi = baseApi.injectEndpoints({
-  endpoints: (builder) => ({
-    getUsers: builder.query({
-      query: () => "users",
-    }),
-  }),
+export const fetchUsers = () => {
+  return async (dispatch) => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      console.log(response);
+      console.log(response.data);
+      dispatch(fetchUsersSuccess(response.data));
+    } catch (error) {
+      dispatch(fetchUsersFailure(error.message));
+    }
+  };
+};
+
+const fetchUsersSuccess = (users) => ({
+  type: "FETCH_USERS_SUCCESS",
+  payload: users,
 });
 
-export const { useGetUsersQuery } = usersApi;
+const fetchUsersFailure = (error) => ({
+  type: "FETCH_USERS_FAILURE",
+  payload: error,
+});
