58bfa78c8f6e250fd1eafeb37fcd39f8bc253885
Volodymyr Donets <volodons@gmail.com>
2023-08-02 14:08:46 +0300

    Reconfigure PostItem.tsx to work with Redux Thunk

--- src/components/post/PostItem.tsx
+++ src/components/post/PostItem.tsx
@@ -1,7 +1,20 @@
-import { useGetPostsQuery } from "../../api/postsApi";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchPosts } from "../../api/postsApi";
 
 function PostItem(): JSX.Element {
-  const { data: posts = [], error } = useGetPostsQuery();
+  const posts = useSelector((state) => state.posts.data);
+  const loading = useSelector((state) => state.posts.loading);
+  const error = useSelector((state) => state.posts.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   if (error) {
     return <div>Error loading posts</div>;
