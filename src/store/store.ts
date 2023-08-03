5f6436466dbd623ce3357b6c6d71fd028f48a3b0
Volodymyr Donets <volodons@gmail.com>
2023-08-03 21:46:24 +0300

    Implement app's state persistence with Redux Persist

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { useDispatch } from "react-redux";
 import postsReducer from "../slices/postsSlice";
 import todosReducer from "../slices/todosSlice";
@@ -10,12 +12,21 @@ const rootReducer = combineReducers({
   users: usersReducer,
 });
 
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+
+const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-export default store;
+export { store, persistor };
