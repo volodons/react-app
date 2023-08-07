33b1a5d7e0ed2d9980c1429e5754202ea7a6d31e
Volodymyr Donets <volodons@gmail.com>
2023-08-07 11:44:47 +0300

    Implement non-rewriting of Redux-Persist Posts data with API calls

--- src/api/postsApi.ts
+++ src/api/postsApi.ts
@@ -1,13 +1,21 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { store } from "../store/store";
 
 export const fetchPosts = createAsyncThunk("fetchPosts", async () => {
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
-    );
-    return response.data;
-  } catch (error) {
-    throw Error("Failed to fetch posts");
+  const state = store.getState();
+  const posts = state.posts.posts;
+
+  if (posts.length === 0) {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      return response.data;
+    } catch (error) {
+      throw Error("Failed to fetch posts");
+    }
+  } else {
+    return posts;
   }
 });
