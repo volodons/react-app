c52536b983eed63a3e038aca27fdf3d2d89805cc
Volodymyr Donets <volodons@gmail.com>
2023-08-07 11:48:12 +0300

    Implement non-rewriting of Redux-Persist Users data with API calls

--- src/api/usersApi.ts
+++ src/api/usersApi.ts
@@ -1,13 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { store } from "../store/store";
 
 export const fetchUsers = createAsyncThunk("fetchUsers", async () => {
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    return response.data;
-  } catch (error) {
-    throw Error("Failed to fetch users");
+  const state = store.getState();
+  const users = state.users.users;
+
+  if (users.length === 0) {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      return response.data;
+    } catch (error) {
+      throw Error("Failed to fetch users");
+    }
+  } else {
+    return users;
   }
 });
