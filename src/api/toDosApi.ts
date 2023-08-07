91a5c5a43cdfc126d59cf14e6890e2508f2979df
Volodymyr Donets <volodons@gmail.com>
2023-08-07 11:42:34 +0300

    Implement non-rewriting of Redux-Persist Todos data with API calls

--- src/api/toDosApi.ts
+++ src/api/toDosApi.ts
@@ -1,13 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import { store } from "../store/store";
 
 export const fetchTodos = createAsyncThunk("fetchTodos", async () => {
-  try {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/todos"
-    );
-    return response.data;
-  } catch (error) {
-    throw Error("Failed to fetch todos");
+  const state = store.getState();
+  const todos = state.todos.todos;
+
+  if (todos.length === 0) {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      return response.data;
+    } catch (error) {
+      throw Error("Failed to fetch todos");
+    }
+  } else {
+    return todos;
   }
 });
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,14 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { useDispatch } from "react-redux";
 import postsReducer from "../slices/postsSlice";
