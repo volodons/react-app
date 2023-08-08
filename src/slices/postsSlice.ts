a0a17b7193a2b8d90fce8151eb3da2b17928c931
Volodymyr Donets <volodons@gmail.com>
2023-08-08 18:22:51 +0300

    Create add post reducer for posts state and extra reducers for post sending

--- src/slices/postsSlice.ts
+++ src/slices/postsSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchPosts } from "../api/postsApi";
+import { sendPost } from "../api/postsApi";
 
 const initialState = {
   posts: [],
@@ -10,7 +11,16 @@ const initialState = {
 const postsSlice = createSlice({
   name: "posts",
   initialState,
-  reducers: {},
+  reducers: {
+    addPost: (state, action) => {
+      const userId = Math.floor(Math.random() * 10) + 1;
+      const id = state.posts.length + 1;
+      const post = action.payload;
+      post.userId = userId;
+      post.id = id;
+      state.posts.push(post);
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchPosts.pending, (state) => {
@@ -24,8 +34,21 @@ const postsSlice = createSlice({
       .addCase(fetchPosts.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
+      })
+      .addCase(sendPost.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(sendPost.fulfilled, (state) => {
+        state.loading = false;
+        // state.posts = state.posts.push(action.payload);
+      })
+      .addCase(sendPost.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
       });
   },
 });
 
+export const { addPost } = postsSlice.actions;
 export default postsSlice.reducer;
