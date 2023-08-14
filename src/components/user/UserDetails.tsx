df3df8098cc83f5b56b15e29ff7a54a93ff6b8fa
Volodymyr Donets <volodons@gmail.com>
2023-08-14 20:24:37 +0300

    Add basic styling and FontAwesome icons for UserDetails component

--- src/components/user/UserDetails.tsx
+++ src/components/user/UserDetails.tsx
@@ -2,6 +2,14 @@ import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchUserDetails } from "../../api/usersApi";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faPhoneVolume,
+  faLocationDot,
+  faGlobe,
+  faBriefcase,
+} from "@fortawesome/free-solid-svg-icons";
 
 function UserDetails(): JSX.Element {
   const dispatch = useDispatch();
@@ -21,10 +29,28 @@ function UserDetails(): JSX.Element {
   }
 
   return (
-    <div>
-      This is User number {userId}, his data is {userDetails.name},
-      {userDetails.address.street}, {userDetails.address.geo.lat}
-    </div>
+    <ul className="mt-16">
+      <li className="mt-4">
+        <FontAwesomeIcon icon={faEnvelope} style={{ color: "#3b82f6" }} />
+        <span className="ml-6 font-bold">{userDetails.email}</span>
+      </li>
+      <li className="mt-4">
+        <FontAwesomeIcon icon={faPhoneVolume} style={{ color: "#3b82f6" }} />
+        <span className="ml-6 font-bold">{userDetails.phone}</span>
+      </li>
+      <li className="mt-4">
+        <FontAwesomeIcon icon={faLocationDot} style={{ color: "#3b82f6" }} />
+        <span className="ml-6 font-bold">{userDetails.address.city}</span>
+      </li>
+      <li className="mt-4">
+        <FontAwesomeIcon icon={faGlobe} style={{ color: "#3b82f6" }} />
+        <span className="ml-6 font-bold">{userDetails.website}</span>
+      </li>
+      <li className="mt-4">
+        <FontAwesomeIcon icon={faBriefcase} style={{ color: "#3b82f6" }} />
+        <span className="ml-6 font-bold">{userDetails.company.name}</span>
+      </li>
+    </ul>
   );
 }
 
