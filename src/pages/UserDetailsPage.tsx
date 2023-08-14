bcb95534b0b71434b4b241a14750159fbf9c2768
Volodymyr Donets <volodons@gmail.com>
2023-08-14 19:49:31 +0300

    Create UserDetailsPage component

---
+++ src/pages/UserDetailsPage.tsx
@@ -0,0 +1,11 @@
+import Navigation from "../components/layout/Navigation";
+
+function UserDetailsPage(): JSX.Element {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+}
+
+export default UserDetailsPage;
