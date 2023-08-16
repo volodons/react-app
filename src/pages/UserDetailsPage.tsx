e80d1e0210e232c29fa12c67e17050a2c6ada334
Volodymyr Donets <volodons@gmail.com>
2023-08-16 14:21:35 +0300

    Implement centralized routing management in a separate file

--- src/pages/UserDetailsPage.tsx
+++ src/pages/UserDetailsPage.tsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from "react-router-dom";
 import Navigation from "../components/layout/Navigation";
 import UserDetails from "../components/user/UserDetails";
 
@@ -6,9 +5,7 @@ function UserDetailsPage(): JSX.Element {
   return (
     <>
       <Navigation />
-      <Routes>
-        <Route path=":userId" element={<UserDetails />} />
-      </Routes>
+      <UserDetails />
     </>
   );
 }
